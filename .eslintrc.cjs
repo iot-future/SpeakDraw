205a76e4e3323fc12619918fbd441d1a74582e4c
cosmozmzhu <cosmozmzhu@tencent.com>
2026-07-12 13:43:06 +0800

    chore: scaffold pnpm monorepo with linting toolchain

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+  },
+  ignorePatterns: ['dist', 'coverage', 'node_modules', '.eslintrc.cjs'],
+};
